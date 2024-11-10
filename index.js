82d62e9c56ae81b89d5521b7713733f6308327b5
Vyacheslav <vyacheslavs010s@gmail.com>
2024-11-10 23:34:05 +0200

    backendv2

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ import express from 'express'
 import mongoose from 'mongoose'
 import router from "./Router.js";
 import cors from 'cors'
-require('dotenv').config();
+import dotenv from 'dotenv';
+dotenv.config();
 
 const app = express()
 const url = process.env.LOGIN_MONGO
